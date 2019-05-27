fe9d94ad06ddd4737b6230950267b607704f7ec2
jxlwqq <jxlwqq@gmail.com>
2019-05-27 21:53:59 +0800

    first commit

---
+++ src/Arithmetic.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Jxlwqq\Arithmetic;
+
+
+class Arithmetic
+{
+    public function sum($x, $y)
+    {
+        return $x + $y;
+    }
+}
---
+++ test/ArithmeticTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Jxlwqq\Arithmetic\Test;
+
+use Jxlwqq\Arithmetic\Arithmetic;
+use PHPUnit\Framework\TestCase;
+
+class ArithmeticTest extends TestCase
+{
+    public $arithmetic;
+    public function setUp()
+    {
+        parent::setUp();
+        $this->arithmetic = new Arithmetic();
+
+    }
+
+    public function testSum()
+    {
+        $x = 1;
+        $y = 2;
+
+        $this->assertEquals(3, $this->arithmetic->sum($x, $y));
+    }
+}
